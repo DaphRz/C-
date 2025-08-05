e6198be81e934d0e68fe7cb1e0ed14c2569b4a0b
DaphRz <159149963+DaphRz@users.noreply.github.com>
2025-08-05 08:52:57 -0300

    primeira aula - agenda telefônica

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class AgendaTelefonica
+    {
+        //Dictionary<string, int> Contatos = new Dictionary<string, int>();
+        private Dictionary<string, int> Contatos = [];
+
+        private string nome;
+        private string num;
+
+        public int MyProperty { get; set; }
+        
+        public void Inserir(string nome,)
+        {
+             
+
+        }
+    }
+}
