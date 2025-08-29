bd092f98ac2b79b9ce214cc583de664eb51ae4b1
DaphRz <159149963+DaphRz@users.noreply.github.com>
2025-08-29 08:54:13 -0300

    segunda aula

---
+++ Butterfly-Catching-Game-MOO-ICT-master/Butterfly Catching Game MOO ICT/Form1.cs
@@ -0,0 +1,31 @@
+namespace Butterfly_Catching_Game_MOO_ICT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GameWindow game = new GameWindow();
+            game.Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
